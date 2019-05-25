152295f38e008a5f632179d3cb2ee40d5b45845c
Jason Moore <j-mo@pop-os.localdomain>
2019-05-25 13:25:08 -0500

    Updates

---
+++ precache-manifest.d77bafeea5da82ccf1741c5aef4f6f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc1523ee035778c2a81a3af27b1e0f7",
+    "url": "/mb-yoga-react/index.html"
+  },
+  {
+    "revision": "5a30e34126b223a78748",
+    "url": "/mb-yoga-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/mb-yoga-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5a30e34126b223a78748",
+    "url": "/mb-yoga-react/static/js/main.b54f717c.chunk.js"
+  },
+  {
+    "revision": "b3a396c998f840e7e329",
+    "url": "/mb-yoga-react/static/js/runtime~main.00c0554f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mb-yoga-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
